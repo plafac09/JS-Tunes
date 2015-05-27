cd7471be7431bb3d2aadbdfb14aacf90e8f47d57
Jule <Jule@Jule-PC>
2015-05-27 09:27:21 +0200

    Variables and SimpleDateFormat

--- src/bl/PlaylistTrackModel.java
+++ src/bl/PlaylistTrackModel.java
@@ -18,47 +18,6 @@ import javax.swing.AbstractListModel;
 public class PlaylistTrackModel extends AbstractListModel
 {
 
-//    private ObservableList<Track> list = FXCollections.observableArrayList();
-//    private ListView view;
-//
-//    public PlaylistTrackModel(ListView view)
-//    {
-//        this.view = view;
-//        view.setItems(list);
-//    }
-//
-//    public void addTrack(Track t)
-//    {
-//        list.add(t);
-//        view.setItems(list);
-//    }
-//
-//    public void removeTrackByObject(Track t)
-//    {
-//        list.remove(t);
-//        view.setItems(list);
-//    }
-//
-//    public void removeTrackByIndex(int index)
-//    {
-//        list.remove(index);
-//        view.setItems(list);
-//    }
-//
-//    @Override
-//    public int getSize()
-//    {
-//        return list.size();
-//    }
-//
-//    @Override
-//    public Object getElementAt(int index)
-//    {
-//        return list.get(index);
-//    }
-//
-//}
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
